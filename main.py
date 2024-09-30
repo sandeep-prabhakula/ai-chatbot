787d2f2a6a42c476d1f81a0e8a7e2519b02b7fa8
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2024-09-30 22:48:46 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+from flask import Flask, jsonify, request, make_response
+from functions_framework import http
+from utils import getOpenAiResponse
+app2 = Flask(__name__)
+
+
+@app2.after_request
+def remove_server_header(response):
+
+    response.headers.pop("Server", None)
+
+    response.headers.pop("X-Powered-By", None)
+    return response
+
+
+@app2.route("/", methods=['POST', "OPTIONS"])
+def process(request):
+
+    try:
+        if request.method == "OPTIONS":
+            return _build_cors_preflight_response()
+        elif request.method != "POST":
+            return jsonify({"error": "INVALID_REQUEST_BODY"}), 405
+        else:
+            data = request.get_json()
+            response = getOpenAiResponse(data['prompt'])
+
+            status = response.get("statusCode")
+
+            if status:
+                status = int(status)
+            else:
+                status = 200
+
+            return _corsify_actual_response(jsonify(response)), status
+    except Exception as e:
+        print(e)
+        final_json = {"error": "Something Went Wrong", "status": 500}
+        return _corsify_actual_response(jsonify(final_json)), 500
+
+
+if __name__ == "__main__":
+    app2.run(host="0.0.0.0", port=8080)
+
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
+
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
---
+++ utils.py
@@ -0,0 +1,34 @@
+from openai import AzureOpenAI
+
+
+
+def getOpenAiResponse(prompt):
+
+    response=""
+    
+    try:
+        client = AzureOpenAI(
+            api_key="ee0b7f3478a547debc73164b67414934",
+            api_version="2024-05-01-preview",
+            azure_endpoint = "https://azureopenaiinstance.openai.azure.com"
+            )
+        
+        airesponse = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                { "role": "system", "content": "You are a helpful assistant." },
+                { "role": "user", "content":  prompt} 
+            ],
+
+        )
+        finalResponse=airesponse.choices[0].message.content
+
+
+        response=finalResponse
+                    
+        
+    except Exception as e:
+        print(e)
+
+
+    return response

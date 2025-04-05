576deb98238b1736f2b998c87f2b5cccae148d32
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2025-04-05 23:44:42 +0530

    reverted to openAI API

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-from openai import OpenAI
-import os
+from openai import AzureOpenAI
+
 
 
 def getOpenAiResponse(prompt):
@@ -7,24 +7,21 @@ def getOpenAiResponse(prompt):
     response={}
     
     try:
-        apiKey = os.getenv("OPENAI_API_KEY")
-        client = OpenAI(
-  base_url="https://openrouter.ai/api/v1",
-  api_key="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",
-)
+        client = AzureOpenAI(
+            api_key="ee0b7f3478a547debc73164b67414934",
+            api_version="2024-05-01-preview",
+            azure_endpoint = "https://azureopenaiinstance.openai.azure.com"
+            )
+        
+        airesponse = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                { "role": "system", "content": "You are a helpful assistant." },
+                { "role": "user", "content":  prompt} 
+            ],
 
-        completion = client.chat.completions.create(
-  extra_body={},
-  model="qwen/qwen2.5-vl-72b-instruct:free",
-#   model="deepseek/deepseek-r1-distill-llama-70b:free",
-  messages=[
-    {
-      "role": "user",
-      "content":prompt
-    }
-  ]
-)
-        finalResponse=completion.choices[0].message.content
+        )
+        finalResponse=airesponse.choices[0].message.content
 
 
         response['response']=finalResponse

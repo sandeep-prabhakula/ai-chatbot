42126c0a5d342228863e02f6bd1105792c900d58
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2025-04-07 19:44:09 +0530

    implemented deepseek R1 model

--- utils.py
+++ utils.py
@@ -7,20 +7,21 @@ def getOpenAiResponse(prompt):
     response={}
     
     try:
-        client = AzureOpenAI(
-            api_key="ee0b7f3478a547debc73164b67414934",
-            api_version="2024-05-01-preview",
-            azure_endpoint = "https://azureopenaiinstance.openai.azure.com"
-            )
-        
-        airesponse = client.chat.completions.create(
-            model="gpt-4o",
-            messages=[
-                { "role": "system", "content": "You are a helpful assistant." },
-                { "role": "user", "content":  prompt} 
-            ],
+        client = OpenAI(
+                    base_url="https://openrouter.ai/api/v1",
+                    api_key="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",
+                )
 
-        )
+        completion = client.chat.completions.create(
+                        extra_body={},
+                        model="deepseek/deepseek-r1-distill-llama-70b:free",
+                        messages=[
+                                {
+                                    "role": "user",
+                                    "content":prompt
+                                }
+                            ]
+                    )
         finalResponse=airesponse.choices[0].message.content
 
 
@@ -28,7 +29,7 @@ def getOpenAiResponse(prompt):
                     
         
     except Exception as e:
-        print(e)
+        print("Exception : "+str(e))
 
 
     return response
